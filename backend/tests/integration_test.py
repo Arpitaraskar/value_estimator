ad40cae8e7c55e87b937bf79b68f4ce6fb750201
Arpita Raskar <raskararpita2005@gmail.com>
2026-08-04 10:48:24 +0530

    integration testing file update

--- backend/tests/integration_test.py
+++ backend/tests/integration_test.py
@@ -1,10 +1,46 @@
+# from fastapi.testclient import TestClient
+# from app.main import app
+
+# client = TestClient(app)
+
+
+# def test_predict_integration():
+
+#     response = client.post(
+#         "/predict",
+#         json={
+#             "MedInc": 1,
+#             "HouseAge": 10,
+#             "AveRooms": 5,
+#             "AveBedrms": 1,
+#             "Population": 100,
+#             "AveOccup": 2,
+#             "Latitude": 34,
+#             "Longitude": -118
+#         }
+#     )
+
+#     assert response.status_code == 200
+
+#     data = response.json()
+
+#     assert "predicted_price" in data
+#     assert "predicted_price_short" in data
+#     assert "confidence_range" in data
+
+#mock test
+
+from unittest.mock import patch
 from fastapi.testclient import TestClient
 from app.main import app
 
 client = TestClient(app)
 
 
-def test_predict_integration():
+@patch("app.services.prediction_service.predict_price")
+def test_predict_integration(mock_predict):
+
+    mock_predict.return_value = 2.5
 
     response = client.post(
         "/predict",
