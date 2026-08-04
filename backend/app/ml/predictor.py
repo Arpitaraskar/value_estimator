a1c4a6ac5cc3106e79aacca93fc26df65bbf784e
Arpita Raskar <raskararpita2005@gmail.com>
2026-08-04 10:43:59 +0530

    update predict.py

--- backend/app/ml/predictor.py
+++ backend/app/ml/predictor.py
@@ -1,22 +1,42 @@
+# import joblib
+# import pandas as pd
+
+# from app.core.config import MODEL_PATH
+
+# model = joblib.load(MODEL_PATH)
 
 
+# def predict_price(house_data: dict):
+
+#     input_df = pd.DataFrame([house_data])
+
+#     prediction = model.predict(input_df)[0]
+
+#     return prediction
 
 import joblib
 import pandas as pd
 
-
 from app.core.config import MODEL_PATH
 
+model = None
 
 
+def load_model():
+    global model
 
-model = joblib.load(MODEL_PATH)
+    if model is None:
+        model = joblib.load(MODEL_PATH)
+
+    return model
 
 
 def predict_price(house_data: dict):
 
+    loaded_model = load_model()
+
     input_df = pd.DataFrame([house_data])
 
-    prediction = model.predict(input_df)[0]
+    prediction = loaded_model.predict(input_df)[0]
 
     return prediction
